bdf694705c762e7519b5fb08a4a0131f6f324294
Michael Spencer <MSpencer87@users.noreply.github.com>
2024-02-01 17:02:40 -0800

    Fixes for agentEnvVars, updates for proxy tests
    Fixes for custom cluster reg strings with env vars
    
    Added helper for setting corral bastion addresses

--- extensions/provisioning/creates.go
+++ extensions/provisioning/creates.go
@@ -552,8 +552,9 @@ func CreateProvisioningAirgapCustomCluster(client *rancher.Client, clustersConfi
 		if err != nil {
 			return nil, err
 		}
-
-		command := fmt.Sprintf("%s %s", token.InsecureNodeCommand, roles)
+		// environment variables must be escaped inside original registration command
+		temp := strings.Replace(token.InsecureNodeCommand, "\"", "\\\"", -1)
+		command := fmt.Sprintf("%s %s", temp, roles)
 		logrus.Infof("registration command is %s", command)
 		err = corral.UpdateCorralConfig("registration_command", command)
 		if err != nil {
@@ -632,7 +633,9 @@ func CreateProvisioningRKE1AirgapCustomCluster(client *rancher.Client, clustersC
 			return nil, err
 		}
 
-		command := fmt.Sprintf("%s %s", token.NodeCommand, roles)
+		// environment variables must be escaped inside original registration command
+		temp := strings.Replace(token.NodeCommand, "\"", "\\\"", -1)
+		command := fmt.Sprintf("%s %s", temp, roles)
 		logrus.Infof("registration command is %s", command)
 		err = corral.UpdateCorralConfig("registration_command", command)
 		if err != nil {
