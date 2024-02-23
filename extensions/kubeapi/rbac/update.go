c6ad0f0f88c993f88ae5dab4aac9c5c89c0dfdaf
Anupama <38144301+anupama2501@users.noreply.github.com>
2024-02-22 16:06:02 -0800

    Adding extensions for the global roles v2 webhook checks

--- extensions/kubeapi/rbac/update.go
+++ extensions/kubeapi/rbac/update.go
@@ -42,3 +42,71 @@ func UpdateGlobalRole(client *rancher.Client, updatedGlobalRole *v3.GlobalRole) 
 	}
 	return newGlobalRole, nil
 }
+
+// UpdateRoleTemplate is a helper function that uses the dynamic client to update an existing cluster role template
+func UpdateRoleTemplate(client *rancher.Client, updatedRoleTemplate *v3.RoleTemplate) (*v3.RoleTemplate, error) {
+	dynamicClient, err := client.GetDownStreamClusterClient(LocalCluster)
+	if err != nil {
+		return nil, err
+	}
+	roleTemplateUnstructured := dynamicClient.Resource(RoleTemplateGroupVersionResource)
+	roleTemplate, err := roleTemplateUnstructured.Get(context.TODO(), updatedRoleTemplate.Name, metav1.GetOptions{})
+	if err != nil {
+		return nil, err
+	}
+
+	currentRoleTemplate := &v3.RoleTemplate{}
+	err = scheme.Scheme.Convert(roleTemplate, currentRoleTemplate, roleTemplate.GroupVersionKind())
+	if err != nil {
+		return nil, err
+	}
+
+	updatedRoleTemplate.ObjectMeta.ResourceVersion = currentRoleTemplate.ObjectMeta.ResourceVersion
+
+	unstructuredResp, err := roleTemplateUnstructured.Update(context.TODO(), unstructured.MustToUnstructured(updatedRoleTemplate), metav1.UpdateOptions{})
+	if err != nil {
+		return nil, err
+	}
+
+	newRoleTemplate := &v3.RoleTemplate{}
+	err = scheme.Scheme.Convert(unstructuredResp, newRoleTemplate, unstructuredResp.GroupVersionKind())
+	if err != nil {
+		return nil, err
+	}
+	return newRoleTemplate, nil
+
+}
+
+// UpdateClusterRoleTemplateBindings is a helper function that uses the dynamic client to update an existing cluster role template binding
+func UpdateClusterRoleTemplateBindings(client *rancher.Client, existingCRTB *v3.ClusterRoleTemplateBinding, updatedCRTB *v3.ClusterRoleTemplateBinding) (*v3.ClusterRoleTemplateBinding, error) {
+	dynamicClient, err := client.GetDownStreamClusterClient(LocalCluster)
+	if err != nil {
+		return nil, err
+	}
+	crtbUnstructured := dynamicClient.Resource(ClusterRoleTemplateBindingGroupVersionResource).Namespace(existingCRTB.Namespace)
+	clusterRoleTemplateBinding, err := crtbUnstructured.Get(context.TODO(), existingCRTB.Name, metav1.GetOptions{})
+	if err != nil {
+		return nil, err
+	}
+
+	currentCRTB := &v3.ClusterRoleTemplateBinding{}
+	err = scheme.Scheme.Convert(clusterRoleTemplateBinding, currentCRTB, clusterRoleTemplateBinding.GroupVersionKind())
+	if err != nil {
+		return nil, err
+	}
+
+	updatedCRTB.ObjectMeta.ResourceVersion = currentCRTB.ObjectMeta.ResourceVersion
+
+	unstructuredResp, err := crtbUnstructured.Update(context.TODO(), unstructured.MustToUnstructured(updatedCRTB), metav1.UpdateOptions{})
+	if err != nil {
+		return nil, err
+	}
+
+	newCRTB := &v3.ClusterRoleTemplateBinding{}
+	err = scheme.Scheme.Convert(unstructuredResp, newCRTB, unstructuredResp.GroupVersionKind())
+	if err != nil {
+		return nil, err
+	}
+	return newCRTB, nil
+
+}
