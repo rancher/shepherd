fff2ff75b282387798ea5b119976f9b8446d14b5
Iramis Valentin <85186645+git-ival@users.noreply.github.com>
2024-02-27 05:26:12 +0000

    ListAll clusters, write kubeconfig to file, write logs to stdout and file, configmap util funcs

--- extensions/kubeconfig/kubeconfig.go
+++ extensions/kubeconfig/kubeconfig.go
@@ -2,8 +2,10 @@ package kubeconfig
 
 import (
 	"errors"
+	"os"
 
 	"github.com/rancher/shepherd/clients/rancher"
+	"github.com/sirupsen/logrus"
 	"k8s.io/client-go/tools/clientcmd"
 )
 
@@ -32,4 +34,29 @@ func GetKubeconfig(client *rancher.Client, clusterID string) (*clientcmd.ClientC
 	}
 
 	return &cfg, nil
+}
+
+func WriteKubeconfigToFile(client *rancher.Client, clusterID, path string) error {
+	cluster, err := client.Management.Cluster.ByID(clusterID)
+	if err != nil {
+		return err
+	}
+
+	kubeConfig, err := client.Management.Cluster.ActionGenerateKubeconfig(cluster)
+	if err != nil {
+		return err
+	}
+
+	_, statErr := os.Stat(path)
+	if statErr == nil {
+		err = os.Remove(path)
+	}
+
+	if f, err := os.Create(path); err == nil {
+		if _, err := f.Write([]byte(kubeConfig.Config)); err != nil {
+			return err
+		}
+	}
+	logrus.Infof("Finished writing. Err: %v", err)
+	return err
 }
