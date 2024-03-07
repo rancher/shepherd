c9a8ad9ab2e9193f6357ef65a4add006b3653f56
Anupama <38144301+anupama2501@users.noreply.github.com>
2024-03-07 14:31:32 -0800

    Revert "Adding extensions for rbac checks"
    This reverts commit a26b3eea8294da2e22bcd7d16a98de0b556475d2.

--- extensions/rbac/config.go
+++ extensions/rbac/config.go
@@ -5,7 +5,7 @@ const (
 )
 
 type Config struct {
-	Role     Role `json:"role" yaml:"role"`
+	Role     string `json:"role" yaml:"role"`
 	Username string `json:"username" yaml:"username"`
 	Password string `json:"password" yaml:"password"`
 }
