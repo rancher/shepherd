70cf58da1d839d3da00f0a8f5638d336ff8e8574
rishabh <rishabh@infracloud.io>
2024-04-15 14:40:17 +0530

    Add docker install support for RKE1 custom cluster

--- extensions/clusters/clusterconfig.go
+++ extensions/clusters/clusterconfig.go
@@ -7,30 +7,31 @@ import (
 )
 
 type ClusterConfig struct {
-	KubernetesVersion    string                                   `json:"kubernetesVersion" yaml:"kubernetesVersion"`
-	CNI                  string                                   `json:"cni" yaml:"cni"`
-	PSACT                string                                   `json:"psact" yaml:"psact"`
-	PNI                  bool                                     `json:"pni" yaml:"pni"`
-	NodePools            []provisioningInput.NodePools            `json:"nodepools" yaml:"nodepools"`
-	MachinePools         []provisioningInput.MachinePools         `json:"machinepools" yaml:"machinepools"`
-	CloudProvider        string                                   `json:"cloudProvider" yaml:"cloudProvider"`
-	Providers            *[]string                                `json:"providers" yaml:"providers"`
-	NodeProviders        *[]string                                `json:"nodeProviders" yaml:"nodeProviders"`
-	Hardened             bool                                     `json:"hardened" yaml:"hardened"`
-	AddOnConfig          *provisioningInput.AddOnConfig           `json:"addonConfig" yaml:"addonConfig"`
-	AgentEnvVars         *[]rkev1.EnvVar                          `json:"agentEnvVars" yaml:"agentEnvVars"`
-	AgentEnvVarsRKE1     *[]management.EnvVar                     `json:"agentEnvVarsRKE1" yaml:"agentEnvVarsRKE1"`
-	ClusterAgent         *management.AgentDeploymentCustomization `json:"clusterAgent" yaml:"clusterAgent"`
-	FleetAgent           *management.AgentDeploymentCustomization `json:"fleetAgent" yaml:"fleetAgent"`
-	ETCD                 *rkev1.ETCD                              `json:"etcd" yaml:"etcd"`
-	ETCDRKE1             *management.ETCDService                  `json:"etcdRKE1" yaml:"etcdRKE1"`
-	LabelsAndAnnotations *provisioningInput.LabelsAndAnnotations  `json:"labelsAndAnnotations" yaml:"labelsAndAnnotations"`
-	Networking           *provisioningInput.Networking            `json:"networking" yaml:"networking"`
-	Registries           *provisioningInput.Registries            `json:"registries" yaml:"registries"`
-	UpgradeStrategy      *rkev1.ClusterUpgradeStrategy            `json:"upgradeStrategy" yaml:"upgradeStrategy"`
-	Advanced             *provisioningInput.Advanced              `json:"advanced" yaml:"advanced"`
-	ClusterSSHTests      []provisioningInput.SSHTestCase          `json:"clusterSSHTests" yaml:"clusterSSHTests"`
-	CRIDockerd           bool                                     `json:"criDockerd" yaml:"criDockerd"`
+	KubernetesVersion              string                                            `json:"kubernetesVersion" yaml:"kubernetesVersion"`
+	CNI                            string                                            `json:"cni" yaml:"cni"`
+	PSACT                          string                                            `json:"psact" yaml:"psact"`
+	PNI                            bool                                              `json:"pni" yaml:"pni"`
+	NodePools                      []provisioningInput.NodePools                     `json:"nodepools" yaml:"nodepools"`
+	MachinePools                   []provisioningInput.MachinePools                  `json:"machinepools" yaml:"machinepools"`
+	CloudProvider                  string                                            `json:"cloudProvider" yaml:"cloudProvider"`
+	Providers                      *[]string                                         `json:"providers" yaml:"providers"`
+	NodeProviders                  *[]string                                         `json:"nodeProviders" yaml:"nodeProviders"`
+	Hardened                       bool                                              `json:"hardened" yaml:"hardened"`
+	AddOnConfig                    *provisioningInput.AddOnConfig                    `json:"addonConfig" yaml:"addonConfig"`
+	AgentEnvVars                   *[]rkev1.EnvVar                                   `json:"agentEnvVars" yaml:"agentEnvVars"`
+	AgentEnvVarsRKE1               *[]management.EnvVar                              `json:"agentEnvVarsRKE1" yaml:"agentEnvVarsRKE1"`
+	ClusterAgent                   *management.AgentDeploymentCustomization          `json:"clusterAgent" yaml:"clusterAgent"`
+	FleetAgent                     *management.AgentDeploymentCustomization          `json:"fleetAgent" yaml:"fleetAgent"`
+	ETCD                           *rkev1.ETCD                                       `json:"etcd" yaml:"etcd"`
+	ETCDRKE1                       *management.ETCDService                           `json:"etcdRKE1" yaml:"etcdRKE1"`
+	LabelsAndAnnotations           *provisioningInput.LabelsAndAnnotations           `json:"labelsAndAnnotations" yaml:"labelsAndAnnotations"`
+	Networking                     *provisioningInput.Networking                     `json:"networking" yaml:"networking"`
+	Registries                     *provisioningInput.Registries                     `json:"registries" yaml:"registries"`
+	UpgradeStrategy                *rkev1.ClusterUpgradeStrategy                     `json:"upgradeStrategy" yaml:"upgradeStrategy"`
+	Advanced                       *provisioningInput.Advanced                       `json:"advanced" yaml:"advanced"`
+	ClusterSSHTests                []provisioningInput.SSHTestCase                   `json:"clusterSSHTests" yaml:"clusterSSHTests"`
+	CRIDockerd                     bool                                              `json:"criDockerd" yaml:"criDockerd"`
+	RKE1CustomClusterDockerInstall *provisioningInput.RKE1CustomClusterDockerInstall `json:"rke1CustomClusterDockerInstall" yaml:"rke1CustomClusterDockerInstall"`
 }
 
 // ConvertConfigToClusterConfig converts the config from (user) provisioning input to a cluster config
@@ -57,6 +58,7 @@ func ConvertConfigToClusterConfig(provisioningConfig *provisioningInput.Config) 
 	newConfig.PSACT = provisioningConfig.PSACT
 	newConfig.PNI = provisioningConfig.PNI
 	newConfig.ClusterSSHTests = provisioningConfig.ClusterSSHTests
+	newConfig.RKE1CustomClusterDockerInstall = provisioningConfig.RKE1CustomClusterDockerInstall
 
 	return &newConfig
 }
