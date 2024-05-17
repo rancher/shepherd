a2504991540bcccc224c60c2deaaf588f62bf64d
Sam Gartner <samuel.gartner@suse.com>
2024-05-17 13:59:36 -0500

    initial commit
    rke2 template base commit
    
    PR updates
    
    More PR updates
    
    Update rke2template.go
    
    PR updates
    
    repositories
    
    Update rke2template.go
    
    Update rke2template.go
    
    Update rke2template.go
    
    Update rke2template.go
    
    Update rke2template.go
    
    config updates
    
    Update wait.go
    
    Generic Create with polling
    
    Update go.mod
    
    Update go.mod
    
    Update steve.go
    
    remove bad merge conflict headers

--- extensions/steve/steve.go
+++ extensions/steve/steve.go
@@ -6,12 +6,14 @@ import (
 	"time"
 
 	"github.com/rancher/shepherd/clients/rancher"
+	v1 "github.com/rancher/shepherd/clients/rancher/v1"
 	"github.com/sirupsen/logrus"
 	kwait "k8s.io/apimachinery/pkg/util/wait"
 )
 
 const (
 	notFound = "not found"
+	active   = "active"
 )
 
 // WaitForSteveResourceDeletion accepts a client, steve resource type, and resource ID, then waits for a steve resource to be deleted
@@ -35,4 +37,46 @@ func WaitForSteveResourceDeletion(client *rancher.Client, interval, timeout time
 	}
 
 	return nil
+}
+
+// WaitForSteveResourceCreation waits for a given steve object to be created/come up active.
+func WaitForSteveResourceCreation(client *rancher.Client, v1Resource *v1.SteveAPIObject, interval, timeout time.Duration) error {
+	err := kwait.PollUntilContextTimeout(context.TODO(), interval, timeout, true, func(ctx context.Context) (done bool, err error) {
+		client, err = client.ReLogin()
+		if err != nil {
+			return false, err
+		}
+
+		clusterResp, err := client.Steve.SteveType(v1Resource.Type).ByID(v1Resource.ID)
+		if err != nil {
+			return false, err
+		}
+
+		if clusterResp.ObjectMeta.State.Name == active {
+			logrus.Infof("%s(%s) is active", v1Resource.Kind, v1Resource.Name)
+			return true, nil
+		}
+
+		return false, nil
+	})
+
+	return err
+}
+
+// CreateResource creates a steve resource and polls the resulting object until it comes up active.
+func CreateAndWaitForResource(client *rancher.Client, v1ResourceType string, v1Resource any, poll bool, interval, timeout time.Duration) (*v1.SteveAPIObject, error) {
+	resource, err := client.Steve.SteveType(v1ResourceType).Create(v1Resource)
+	if err != nil {
+		return nil, err
+	}
+	logrus.Infof("Creating %s(%s)", resource.Kind, resource.Name)
+
+	if poll {
+		err := WaitForSteveResourceCreation(client, resource, interval, timeout)
+		if err != nil {
+			return resource, err
+		}
+	}
+
+	return resource, nil
 }
