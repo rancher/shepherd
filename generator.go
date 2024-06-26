a66b4c124825775d42df9f3e7f84e6d5fcd12542
Daniel Newman <dan@danlovesprogramming.com>
2024-06-26 14:19:20 -0500

    Update ClusterRepo and App controller interfaces
    This commit updates the interfaces for ClusterRepo and App controllers in the catalog.cattle.io package. The old methods have been replaced with more abstract methods from the wrangler v2 package. The codebase has been simplified significantly, making it easier to add methods in the future. In addition, the copyright year has been updated to 2024.
    
    Added documentation
    
    Refactor test session related functions in codegen
    
    The code has been refactored to generalize the function of adding test sessions to different controllers. The functions for importing test session in each controller were renamed and refactored into a unified function, which is now called inside each controller-specific function. This approach aims to reduce code repetition and complexity.
    
    Add session to struct and function in generated files
    
    Expanded the appendSessionToStructInFile function to appendNewlineToBlockInFile, which generalizes line additions to the end of any struct or function block. This update improves code reusability. Utilized this function to add a session to multiple generated interface files.
    
    Add session to struct in management controllers
    
    A new function has been implemented to append a session to a struct in the management controllers. The function reads a file, locates a given struct, and then adds a new session prior to the closing brace of the struct. This change will be beneficial for the management of sessions within the struct.
    
    Refactor import addition function for error handling
    
    Updated the addImport function in codegen/main.go to handle and return errors instead of triggering panic. This change also includes verification of the import path and logic adjustments for adding a new import to the parsed file. These updates ensure more robust and error-resistant code execution.
    
    Add 'session' import to controller files
    
    This commit introduces additional functions to automatically add 'session' imports into controller files. Functions have been added to process and add this import into management, apps, and core controller files specifically. The new functions will walk the respective directory's file-paths and add the specified import to all 'interface.go' files.
    
    Refactor factory method parameter replacement in codegen
    
    The factory methods in the code generator have been refactored. The parameters passed into these methods have been modified to include the timestamp (TS) option. Also, excess white space in the code has been removed to adhere to clean coding principles.

--- generator.go
+++ generator.go
@@ -2,5 +2,6 @@
 //go:generate go run pkg/codegen/generator/cleanup/main.go -mod vendor
 //go:generate go run pkg/codegen/main.go -mod vendor
 //go:generate rm -rf vendor
+//go:generate go fmt ./...
 
 package main
