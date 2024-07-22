7445af5e4f8ae568d72582addaf3637921a26129
Sam Gartner <samuel.gartner@suse.com>
2024-07-22 12:13:02 -0500

    Update cloud credentials
    Convert V3-->V1 objects and also move the loadConfig call outside of the cloud credentials create as it is bad practice to:
    1. Hardcode a load config in functional logic that gets reused (reduces reusability)
    2. Combine crud operations. (in this case get and create)
    
    Update generated name with package const
    
    update generate name and spacing
    
    update DO
    
    Update create.go
    
    Update create.go
    
    Update digitalocean_machine_config.go
    
    Update the credentials with the new client.UserID field
    
    Update cloudcredentials.go
    
    Update cloudcredentials.go
    
    Update after actions vs extentions

--- extensions/cloudcredentials/linode/create.go
+++ extensions/cloudcredentials/linode/create.go
@@ -2,28 +2,40 @@ package linode
 
 import (
 	"github.com/rancher/shepherd/clients/rancher"
-	management "github.com/rancher/shepherd/clients/rancher/generated/management/v3"
+	v1 "github.com/rancher/shepherd/clients/rancher/v1"
 	"github.com/rancher/shepherd/extensions/cloudcredentials"
-	"github.com/rancher/shepherd/pkg/config"
+	"github.com/rancher/shepherd/extensions/defaults"
+	"github.com/rancher/shepherd/extensions/defaults/namespaces"
+	"github.com/rancher/shepherd/extensions/defaults/providers"
+	"github.com/rancher/shepherd/extensions/defaults/stevetypes"
+	"github.com/rancher/shepherd/extensions/steve"
+	"github.com/rancher/shepherd/pkg/namegenerator"
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
-const linodeCloudCredNameBase = "linodeCloudCredential"
-
-// CreateLinodeCloudCredentials is a helper function that takes the rancher Client as a parameter and creates
-// a Linode cloud credential, and returns the CloudCredential response
-func CreateLinodeCloudCredentials(rancherClient *rancher.Client) (*cloudcredentials.CloudCredential, error) {
-	var linodeCredentialConfig cloudcredentials.LinodeCredentialConfig
-	config.LoadConfig(cloudcredentials.LinodeCredentialConfigurationFileKey, &linodeCredentialConfig)
-
-	cloudCredential := cloudcredentials.CloudCredential{
-		Name:                   linodeCloudCredNameBase,
-		LinodeCredentialConfig: &linodeCredentialConfig,
+// CreateLinodeCloudCredentials is a helper function that creates V1 cloud credentials and waits for them to become active.
+func CreateLinodeCloudCredentials(client *rancher.Client, credentials cloudcredentials.CloudCredential) (*v1.SteveAPIObject, error) {
+	secretName := namegenerator.AppendRandomString(providers.Linode)
+	spec := corev1.Secret{
+		ObjectMeta: metav1.ObjectMeta{
+			GenerateName: cloudcredentials.GeneratedName,
+			Namespace:    namespaces.CattleData,
+			Annotations: map[string]string{
+				"field.cattle.io/name":      secretName,
+				"field.cattle.io/creatorId": client.UserID,
+			},
+		},
+		Data: map[string][]byte{
+			"linodecredentialConfig-token": []byte(credentials.LinodeCredentialConfig.Token),
+		},
+		Type: corev1.SecretTypeOpaque,
 	}
 
-	resp := &cloudcredentials.CloudCredential{}
-	err := rancherClient.Management.APIBaseClient.Ops.DoCreate(management.CloudCredentialType, cloudCredential, resp)
+	linodeCloudCredentials, err := steve.CreateAndWaitForResource(client, stevetypes.Secret, spec, true, defaults.FiveSecondTimeout, defaults.FiveMinuteTimeout)
 	if err != nil {
 		return nil, err
 	}
-	return resp, nil
+
+	return linodeCloudCredentials, nil
 }
