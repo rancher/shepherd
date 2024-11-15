40b2d04c283078915c9b0a4a68c05f30d8dd9f99
Priyashetty17 <priyanka.shetty@suse.com>
2024-11-15 16:17:49 -0500

    Add config variables for registry secret credentials

--- clients/rancher/config.go
+++ clients/rancher/config.go
@@ -5,14 +5,17 @@ const ConfigurationFileKey = "rancher"
 
 // Config is configuration need to test against a rancher instance
 type Config struct {
-	Host          string `yaml:"host" json:"host"`
-	AdminToken    string `yaml:"adminToken" json:"adminToken"`
-	AdminPassword string `yaml:"adminPassword" json:"adminPassword"`
-	Insecure      *bool  `yaml:"insecure" json:"insecure" default:"true"`
-	Cleanup       *bool  `yaml:"cleanup" json:"cleanup" default:"true"`
-	CAFile        string `yaml:"caFile" json:"caFile" default:""`
-	CACerts       string `yaml:"caCerts" json:"caCerts" default:""`
-	ClusterName   string `yaml:"clusterName" json:"clusterName" default:""`
-	ShellImage    string `yaml:"shellImage" json:"shellImage" default:""`
-	RancherCLI    bool   `yaml:"rancherCLI" json:"rancherCLI" default:"false"`
+	Host             string `yaml:"host" json:"host"`
+	AdminToken       string `yaml:"adminToken" json:"adminToken"`
+	AdminPassword    string `yaml:"adminPassword" json:"adminPassword"`
+	Insecure         *bool  `yaml:"insecure" json:"insecure" default:"true"`
+	Cleanup          *bool  `yaml:"cleanup" json:"cleanup" default:"true"`
+	CAFile           string `yaml:"caFile" json:"caFile" default:""`
+	CACerts          string `yaml:"caCerts" json:"caCerts" default:""`
+	ClusterName      string `yaml:"clusterName" json:"clusterName" default:""`
+	ShellImage       string `yaml:"shellImage" json:"shellImage" default:""`
+	RancherCLI       bool   `yaml:"rancherCLI" json:"rancherCLI" default:"false"`
+	Registry         string `yaml:"registry" json:"registry" default:""`
+	RegistryUsername string `yaml:"registryUsername" json:"registryUsername" default:""`
+	RegistryPassword string `yaml:"registryPassword" json:"registryPassword" default:""`
 }
